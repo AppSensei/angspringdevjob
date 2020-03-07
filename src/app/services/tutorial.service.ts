0090d0807444b717efa18f1725a4d7e52eea5a4d
MidasTouch <ronyjohn007@gmail.com>
2020-03-07 10:41:34 -0500

    Pushing all changes

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Book} from '../Model/Book';
 
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = 'http://ec2-18-215-122-72.compute-1.amazonaws.com';
 
 @Injectable({
   providedIn: 'root'
